e5954d0e5ce327b0eb5446f193149349c0fdcc39
CuongPham15196 <pdcuong1996@gmail.com>
2021-05-22 12:20:30 +0700

    upload

--- src/Route/index.jsx
+++ src/Route/index.jsx
@@ -6,6 +6,14 @@ import Login from "Pages/HomeTemplate/Login";
 import CinemaMobile from "Pages/HomeTemplate/CinemaMobile";
 import TicketOffice from "Pages/HomeTemplate/TicketOffice";
 import DashBoard from "Pages/AdminTemplate/DashBoard";
+import AddUser from "Pages/AdminTemplate/AddUser";
+import ListUser from "Pages/AdminTemplate/ListUser";
+import AddMovie from "Pages/AdminTemplate/AddMovie";
+import AddTicket from "Pages/AdminTemplate/AddTicket";
+import ListMoviePage from "Pages/AdminTemplate/ListMovie";
+import ListTicket from "Pages/AdminTemplate/ListTicket";
+
+import ListUserPagination from "Pages/AdminTemplate/ListUserPagination";
 
 const routerHome = [
   {
@@ -49,6 +57,42 @@ const routerAdmin = [
   {
     path: "/dash-board",
     component: DashBoard,
+    exact: false,
+  },
+  {
+    path: "/add-user",
+    component: AddUser,
+    exact: false,
+  },
+  // {
+  //   path: "/list-user",
+  //   component: ListUser,
+  //   exact: false,
+  // },
+  {
+    path: "/add-movie",
+    component: AddMovie,
+    exact: false,
+  },
+
+  {
+    path: "/add-ticket",
+    component: AddTicket,
+    exact: false,
+  },
+  {
+    path: "/list-movie",
+    component: ListMoviePage,
+    exact: false,
+  },
+  {
+    path: "/list-user-pagination",
+    component: ListUserPagination,
+    exact: false,
+  },
+  {
+    path: "/list-ticket",
+    component: ListTicket,
     exact: false,
   },
 ];
